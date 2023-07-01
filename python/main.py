4cf0592dac983b203a72442fab5ec2ce78198c32
Danijel Korent <danijel.korent@gmail.com>
2023-07-01 18:38:42 +0200

    Initial TODO comments

--- python/main.py
+++ python/main.py
@@ -1,5 +1,14 @@
 #! /usr/bin/env python3
- 
+
+# Implementing RV32IM (Integer + multiplication/division)
+
+# TODO
+#   - Make a list of instructions to implement
+#       - Make a list of "base" instructions
+#       - Make a list of I instructions
+#       - Make a list of M instructions
+
+
 if __name__ == '__main__':
     pass
 
