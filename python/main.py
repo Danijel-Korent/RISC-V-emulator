ae87ab1ed7dbc440f349b65c06567f5183942d24
Danijel Korent <danijel.korent@gmail.com>
2023-07-06 15:51:47 +0200

    Added links to articles about RISC-V ISA

--- python/main.py
+++ python/main.py
@@ -8,6 +8,13 @@
 #       - Make a list of I instructions
 #       - Make a list of M instructions
 
+# DOCU:
+#   - https://itnext.io/risc-v-instruction-set-cheatsheet-70961b4bbe8
+#   - https://en.wikipedia.org/wiki/RISC-V#Design
+#   - https://fraserinnovations.com/risc-v/risc-v-instruction-set-explanation/
+#   - https://riscv.org/wp-content/uploads/2017/05/riscv-spec-v2.2.pdf
+
+
 
 if __name__ == '__main__':
     pass
