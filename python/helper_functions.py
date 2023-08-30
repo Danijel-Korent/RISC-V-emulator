ab9118a53d8d7cf8f528615aab6f2b03e0a61ee3
Danijel Korent <danijel.korent@gmail.com>
2023-08-30 11:06:32 +0200

    BUGFIX: Fixed JAL/JALR not interpreting their immediate values as a signed value

--- python/helper_functions.py
+++ python/helper_functions.py
@@ -9,6 +9,15 @@ def interpret_as_32_bit_signed_value(signed_value):
     return ret_val
 
 
+def interpret_as_20_bit_signed_value(signed_value):
+    ret_val = signed_value
+
+    if signed_value & 0x00100000 != 0:
+        ret_val = -((~signed_value & 0x000FFFFF) + 1)
+
+    return ret_val
+
+
 def interpret_as_12_bit_signed_value(signed_value):
     ret_val = signed_value
 
